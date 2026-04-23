e2227561ba83e8cc5fe424ba0ab1781979718425
Starr <mockup345@gmail.com>
2026-04-23 15:42:28 -0500

    started Now Playing UI

--- apollo/NowPlayingView.swift
+++ apollo/NowPlayingView.swift
@@ -8,7 +8,42 @@
 import SwiftUI
 
 struct NowPlayingView: View {
+    @State private var artworkWidth: CGFloat = .zero
+
     var body: some View {
-        //
+        ZStack {
+            Color.Backgrounds.primary.ignoresSafeArea()
+            
+            VStack(spacing: 8) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.fill.tertiary)
+                    .onGeometryChange(
+                        for: CGFloat.self,
+                        of: { geometry in
+                            geometry.size.width
+                        }
+                    ) { width in
+                        artworkWidth = width
+                    }
+                    .frame(width: artworkWidth, height: artworkWidth)
+                    .overlay {
+                        Image(ImageResource.Bubbly.musicNoteBeamed)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundStyle(Color.Backgrounds.secondary)
+                            .frame(width: 96, height: 96)
+                    }
+                  
+                Spacer()
+
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Title")
+                        .fontWeight(.bold)
+                }
+            }
+            .fontWeight(.medium)
+            .fontDesign(.rounded)
+            .foregroundStyle(Color.Labels.primary)
+        }
     }
 }
