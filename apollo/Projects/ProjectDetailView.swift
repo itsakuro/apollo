ce753ee206d6f4375e7c4acb41c5b8235d3a1bf9
Starr <mockup345@gmail.com>
2026-05-20 09:42:12 -0500

    Added project detail view Not connected to any views yet, will do when I have access to my Mac

---
+++ apollo/Projects/ProjectDetailView.swift
@@ -0,0 +1,77 @@
+//
+//  ProjectDetailView.swift
+//  apollo
+//
+//  Created by Jason Yamoah on 5/19/26.
+//
+
+import SwiftUI
+
+struct ProjectDetailView: View {
+    @Bindable var project: Project
+
+    @State private var artworkWidth: CGFloat = .zero
+
+    var body: some View {
+        NavigationStack {
+            GeometryReader { geometry in
+                VStack(spacing: 28) {
+                    Image(ImageResource.LE_SSERAFIM_DIFFERENT)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: artworkWidth, height: artworkWidth)
+                        .clipShape(.rect(cornerRadius: 26))
+                        .glassEffect(.regular, in: .rect(cornerRadius: 26))
+                        .onAppear {
+                            if geometry.isLandscape {
+                                artworkWidth = geometry.size.height - 92
+                            } else {
+                                artworkWidth = geometry.size.width - 48
+                            }
+                        }
+                        .onChange(of: geometry.size.width) { _, newWidth in
+                            if geometry.isLandscape {
+                                artworkWidth = geometry.size.height - 92
+                            } else {
+                                artworkWidth = geometry.size.width - 48
+                            }
+                        }
+                    
+                    VStack(spacing: 0) {
+                        HStack(spacing: 4) {
+                            Text("DIFFERENT - Single")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            
+                            Image(ImageResource.Bubbly.explicitFill)
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(Color.NowPlaying.Labels.secondary)
+                                .frame(width: 20, height: 20)
+                                .blendMode(.plusLighter)
+                                .allowedDynamicRange(.standard)
+                        }
+
+                        Text("LE SSERAFIM")
+                            .foregroundStyle(Color.NowPlaying.Labels.secondary)
+                            .blendMode(.plusLighter)
+                            .allowedDynamicRange(.standard)
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button {
+                        print("Search in project")
+                    } label: {
+                        Label("Search", systemImage: "magnifyingglass")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProjectDetailView()
+}
